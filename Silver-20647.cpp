39e0c112e5dbcbd7246cc999229cb1ff1e310947
jcho49614 <jcho49614@gmail.com>
2025-11-23 20:58:28 +0900

    silver-20647 done

--- Silver-20647.cpp
+++ Silver-20647.cpp
@@ -1,55 +1,58 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
 
-//linked list
-
 vector<int> v[100005];
+int visarr[100005];
 
 int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
     int n; cin >> n;
-    for(int i =0 ; i < n; i++){
+    for(int i =0; i < n-1; i++)
+    {
         int a, b; cin >> a >> b;
         v[a].push_back(b);
         v[b].push_back(a);
     }
 
-    //Comon root is 1.
+    //linked list.
 
-    queue<int> q; 
+    queue<int> q;
     q.push(1);
 
-    int totalcount = n -1; //totalcount, we nwill add the others to it.
-    
+
+    int cnt = 0;
+
     while(!q.empty()){
+        //the first value of q is the parent.
+        //we need to take out the parent, and find its children
+        //after we found its children, we add them to a tempcnt
         int a = q.front();
         q.pop();
-        //now there is the index 1. we popped that, and find the connecting values. 1 represents the initial vertex. Now we need to find the connecting values.
+        visarr[a] = 1;
+        int totalcnt = 0;
+        int tempcnt = 0;
+        int b = 1;
+       
 
-        int tmpcnt = 0;
-        for(int i : v[a]){
-            //we need to count the amount of cows required.
-            tmpcnt++;
+        int slavecount = 0;
+
+        for(int i =0 ; i < v[a].size(); i++){
+            if(visarr[v[a][i]]==0) slavecount++;
+            if (visarr[v[a][i]] == 0) q.push(v[a][i]);
         }
 
-        if(tmpcnt % 2 == 0){
-            while (tmpcnt != 1){
-                totalcount++;
-                tmpcnt /= 2;
-            }
-        }
-        else{
-            while (tmpcnt !=1){
-                totalcount++;
-                tmpcnt /= 2;
-            }
-            totalcount++;
+         while(b <= slavecount){
+            b *= 2;
+            tempcnt++;
         }
 
+        totalcnt = tempcnt + slavecount;
+        cnt += totalcnt;
     }
 
-    cout << totalcount << "\n";
-
-
+    cout << cnt << "\n";
 }
